45ca7172f2fdf7aaec4f11debfdad2cef8d7af2c
Aliaksei <aliaksei_zhuk@epam.com>
2024-03-08 14:48:53 +0100

    feat: store PORT in env file, add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,19 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { ArtistController } from './artist/artist.controller';
 import { ArtistModule } from './artist/artist.module';
-import { TrackService } from './track/track.service';
 import { TrackModule } from './track/track.module';
-import { AlbumController } from './album/album.controller';
-import { FavoritesController } from './favorites/favorites.controller';
 import { FavoritesModule } from './favorites/favorites.module';
-import { AlbumService } from './album/album.service';
 import { AlbumModule } from './album/album.module';
-import { ArtistService } from './artist/artist.service';
-import { FavoritesService } from './favorites/favorites.service';
-import { TrackController } from './track/track.controller';
 import { USerModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [USerModule, ArtistModule, TrackModule, FavoritesModule, AlbumModule],
+  imports: [
+    USerModule,
+    ArtistModule,
+    TrackModule,
+    FavoritesModule,
+    AlbumModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
